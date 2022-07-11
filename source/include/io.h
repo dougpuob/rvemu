7deb1d51938b1da4f8206f14e158c6a4f50bc7c1
Douglas Chen <dougpuob@gmail.com>
2022-07-11 09:23:45 +0800

    WIP

--- source/include/io.h
+++ source/include/io.h
@@ -1,1 +1,42 @@
 #pragma once
+#include <cstdint>
+#include <vector>
+
+namespace rv64emu {
+
+typedef struct {
+  uint8_t data[0x10000];
+} chunk_t;
+
+// typedef struct {
+//   chunk_t *chunks[0x10000];
+// } memory_t;
+
+template <class T> class Memory {
+private:
+  std::vector<chunk_t *> m_Data;
+
+public:
+  Memory() {
+    m_Data.resize((0x10000));
+    memset(m_Data.data(), 0, m_Data.size() * sizeof(chunk_t *));
+  }
+  ~Memory() {
+    for (auto &chunk : m_Data)
+      if (chunk != nullptr) {
+        delete[] chunk;
+        chunk = nullptr;
+      }
+  }
+  uint64_t ReadStr(uint8_t *dst, T addr, T max);
+  uint64_t FetchInst(T addr);
+  uint64_t Read64(T addr);
+  uint32_t Read32(T addr);
+  uint16_t Read16(T addr);
+  uint8_t Read8(T addr);
+  void Read(uint8_t *dst, T addr, T size);
+  void Write(T addr, uint8_t *dst, T size);
+  void Fill(T addr, T size, uint8_t val);
+};
+
+} // namespace rv64emu
---
+++ source/io.cpp
@@ -0,0 +1,24 @@
+#include "io.h"
+#include <cstdint>
+
+namespace rv64emu {
+
+static chunk_t *allocateChunk() {
+  chunk_t *chunks = new chunk_t;
+  memset(chunks, 0, sizeof(chunk_t));
+  return chunks;
+}
+
+template <class T> uint64_t Memory<T>::ReadStr(uint8_t *dst, T addr, T max) {
+  return 0;
+}
+template <typename T> uint64_t Memory<T>::FetchInst(T addr) { return 0; }
+template <typename T> uint64_t Memory<T>::Read64(T addr) { return 0; }
+template <typename T> uint32_t Memory<T>::Read32(T addr) { return 0; }
+template <typename T> uint16_t Memory<T>::Read16(T addr) { return 0; }
+template <typename T> uint8_t Memory<T>::Read8(T addr) { return 0; }
+template <typename T> void Memory<T>::Read(uint8_t *dst, T addr, T size) {}
+template <typename T> void Memory<T>::Write(T addr, uint8_t *dst, T size) {}
+template <typename T> void Memory<T>::Fill(T addr, T size, uint8_t val) {}
+
+} // namespace rv64emu
